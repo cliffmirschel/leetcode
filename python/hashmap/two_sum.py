a8dcf4555cc8db3cf068fdbc16da488cd246cae3
cliffmirschel <cliffmirschel@yahoo.com>
2024-03-25 15:23:59 -0400

    Added

---
+++ python/hashmap/two_sum.py
@@ -0,0 +1,13 @@
+class Solution:
+    def twoSum(self, nums: list[int], target: int) -> list[int]:
+        cache = {}
+
+        for i, num in enumerate(nums):
+            complement = target - num
+            if complement in cache:
+
+                return [cache[complement], i]
+            
+            cache[num] = i
+
+        return []
