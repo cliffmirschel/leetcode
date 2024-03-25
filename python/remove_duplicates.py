f0f58fd041b51adb9c52c2cb434a474a85908db7
cliffmirschel <cliffmirschel@yahoo.com>
2024-03-25 14:30:11 -0400

    added solution

--- python/remove_duplicates.py
+++ python/remove_duplicates.py
@@ -1,7 +1,8 @@
+from collections import Counter
 # First attempt -> Passed
 
 class Solution:
-    def findDuplicates(self, nums: List[int]) -> List[int]:
+    def findDuplicates(self, nums: list[int]) -> list[int]:
         seen = {}
         res = []
 
@@ -12,3 +13,11 @@ class Solution:
                 seen[num] = True
         
         return res
+
+# Optimzed solution
+    
+def find_duplicates(nums: list):
+    c = Counter(nums)
+    return [k for k, v in c.items() if v > 1]
+
+print(find_duplicates([4,3,2,7,8,2,3,1]))
