c95b5ad6cdd664619c6f5045036cdf90f450cdb1
cliffmirschel <cliffmirschel@yahoo.com>
2024-03-28 19:55:44 -0400

    Added another solution

--- python/array/remove_element.py
+++ python/array/remove_element.py
@@ -1,3 +1,5 @@
+# Optimized Solution
+
 class Solution:
     def removeElement(self, nums: list[int], val: int) -> int:
         count = 0
@@ -11,3 +13,8 @@ class Solution:
 
 
         return count
+
+def remove_element(nums: list[int], val:int):
+    while val in nums:
+        nums.remove(val)
+     
